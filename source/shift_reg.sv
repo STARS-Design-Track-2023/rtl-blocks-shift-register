0f4706b98f3e20f6cec696f0acd00694da3661a6
lagudu-glitch <79684098+lagudu-glitch@users.noreply.github.com>
2023-06-05 16:44:35 -0400

    Edit src

--- source/shift_reg.sv
+++ source/shift_reg.sv
@@ -1,12 +1,9 @@
 module shift_reg
-#(
-    parameter NUM_BITS = 8
-) 
 (
-    input  logic                  clk, nrst, D,
-    input  logic [1:0]            mode_i,
-    input  logic [(NUM_BITS-1):0] par_i,
-    output logic [(NUM_BITS-1):0] P
+    input  logic       clk, nrst, D,
+    input  logic [1:0] mode_i,
+    input  logic [7:0] par_i,
+    output logic [7:0] P
 );
 
     // Write code here
